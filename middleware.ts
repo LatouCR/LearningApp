adaa2be4a33abb2039da0c571419a2f5433583af
LatouCR <dlatouches811@ulacit.ed.cr>
2024-03-13 00:16:33 -0600

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
 import { updateSession } from '@/utils/middleware'
 import { NextRequest } from 'next/server'
 import readUserSession from "@/lib/readUserSession";
-import { redirect } from "next/navigation";
 
 export async function middleware(request: NextRequest) {
   const data = await readUserSession()
-  if (!data) {
-    return redirect('/logIn')
-  }
   return await updateSession(request)
 }
 
